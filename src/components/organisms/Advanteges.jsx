a9a6eefc2999b0cc4ef06a804e533b1afbcb25da
nathaneleven <nathan.martins@elevenrush.com>
2021-11-26 15:01:21 -0300

    Correção de Avantages - LP Optimum

--- src/components/organisms/Advanteges.jsx
+++ src/components/organisms/Advanteges.jsx
@@ -15,19 +15,29 @@ const Advanteges = () => {
           <Image src="/Flexibility.svg"/>
         </Box>
 
-        <Heading pb="10px" as="h3" fontSize="2xl">Ziply Fiber 500/500</Heading>
-        <Text>Get a fast and reliable connection with whole home Wi-Fi on our 100% fiber network. </Text>
+        <Heading pb="10px" as="h3" fontSize="2xl">Speed & Reliability</Heading>
+        <Text>Get a fast and reliable connection with whole home Wi-Fi on our 100% fiber network.</Text>
       </Box>
     </Flex>
     
     <Flex rounded="md" boxShadow="xl" bgColor="#F1F1F1" align="center" direction="column" >
       <Box bgColor="#F1F1F1" color="#000" w="100%" mb="" p="5">
         <Box pb="5">
-          <Image src="/Flexibility.svg"/>
+          <Image src="/Savings.svg"/>
         </Box>
 
-        <Heading pb="10px" as="h3" fontSize="2xl">Ziply Fiber 500/500</Heading>
-        <Text>Get a fast and reliable connection with whole home Wi-Fi on our 100% fiber network. </Text>
+        <Heading pb="10px" as="h3" fontSize="2xl">Value & Transparency</Heading>
+        <Text>Enjoy low prices year after year, with no hidden fees and no surprises.</Text>
+      </Box>
+    </Flex>
+    <Flex rounded="md" boxShadow="xl" bgColor="#F1F1F1" align="center" direction="column" >
+      <Box bgColor="#F1F1F1" color="#000" w="100%" mb="" p="5">
+        <Box pb="5">
+          <Image src="/Protection.svg"/>
+        </Box>
+
+        <Heading pb="10px" as="h3" fontSize="2xl">Risk-Free Experience</Heading>
+        <Text>We install on-time, or pay you $20, and promise satisfaction with a 60-day money-back guarantee.</Text>
       </Box>
     </Flex>
     <Flex rounded="md" boxShadow="xl" bgColor="#F1F1F1" align="center" direction="column" >
@@ -36,18 +46,8 @@ const Advanteges = () => {
           <Image src="/Flexibility.svg"/>
         </Box>
 
-        <Heading pb="10px" as="h3" fontSize="2xl">Ziply Fiber 500/500</Heading>
-        <Text>Get a fast and reliable connection with whole home Wi-Fi on our 100% fiber network. </Text>
-      </Box>
-    </Flex>
-    <Flex rounded="md" boxShadow="xl" bgColor="#F1F1F1" align="center" direction="column" >
-      <Box bgColor="#F1F1F1" color="#000" w="100%" mb="" p="5">
-        <Box pb="5">
-          <Image src="/Flexibility.svg"/>
-        </Box>
-
-        <Heading pb="10px" as="h3" fontSize="2xl">Ziply Fiber 500/500</Heading>
-        <Text>Get a fast and reliable connection with whole home Wi-Fi on our 100% fiber network. </Text>
+        <Heading pb="10px" as="h3" fontSize="2xl">Flexibility</Heading>
+        <Text>Freedom to choose what works for you and change anytime without a contract or penalties</Text>
       </Box>
     </Flex>
   </SimpleGrid>
